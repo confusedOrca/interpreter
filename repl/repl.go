4c6891c3068baefaad578e744a1853283c644551
Al Ibne Siam <confusedorca009@gmail.com>
2024-07-02 20:49:36 +0600

    some changes

--- repl/repl.go
+++ repl/repl.go
@@ -22,7 +22,7 @@ func Start(in io.Reader, out io.Writer) {
 		}
 
 		line := scanner.Text()
-		l := lexer.NewLexer(line)
+		l := lexer.New(line)
 
 		for tkn := l.NextToken(); tkn.Type != token.EOF; tkn = l.NextToken() {
 			fmt.Printf("%+v\n", tkn)
