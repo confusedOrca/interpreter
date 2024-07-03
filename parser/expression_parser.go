5ed8e1373c932b76ef615809a93f88191a902d08
Al Ibne Siam <confusedorca009@gmail.com>
2024-07-04 00:51:37 +0600

    parser package refactoring

--- parser/expression_parser.go
+++ parser/expression_parser.go
@@ -2,6 +2,7 @@ package parser
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/confusedOrca/interpreter/ast"
 	"github.com/confusedOrca/interpreter/token"
@@ -36,4 +37,22 @@ func (p *Parser) parseExpression(precedence int) ast.Expression {
 func (p *Parser) noPrefixParseFnError(t token.TokenType) {
 	msg := fmt.Sprintf("no prefix parse function for %s found", t)
 	p.errors = append(p.errors, msg)
+}
+
+// ------------------------
+// Integer Literal Parser
+// ------------------------
+
+func (p *Parser) parseIntegerLiteral() ast.Expression {
+	lit := &ast.IntegerLiteral{Token: p.curToken}
+	value, err := strconv.ParseInt(p.curToken.Literal, 0, 64)
+
+	if err != nil {
+		msg := fmt.Sprintf("could not parse %q as integer", p.curToken.Literal)
+		p.errors = append(p.errors, msg)
+		return nil
+	}
+
+	lit.Value = value
+	return lit
 }
